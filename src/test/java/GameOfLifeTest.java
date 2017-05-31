2fd889e357d5da59d73cfa49499eacbed98feec5
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-31 10:13:23 +0200

    Add failing test for world Rendering

---
+++ src/test/java/GameOfLifeTest.java
@@ -0,0 +1,44 @@
+import junitparams.JUnitParamsRunner;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by vasilis on 30-5-17.
+ */
+
+@RunWith(JUnitParamsRunner.class)
+public class GameOfLifeTest {
+    GameOfLife SUT;
+    int[][] startingGrid;
+
+    @Before
+    public void setUp() {
+        startingGrid = new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        SUT = new GameOfLife(startingGrid);
+        //Provide the two-dimensional world;
+    }
+
+    @Test
+    public void renderWorld() {
+        //Arrange
+        String expected = "----------\n" +
+                          "----------\n" +
+                          "------*---\n" +
+                          "-----**---\n" +
+                          "----------\n";
+        //Act
+        String actual = SUT.renderGrid(startingGrid);
+
+        //Assert
+        Assert.assertEquals("The world needs to be rendered properly.", expected, actual);
+    }
+}
