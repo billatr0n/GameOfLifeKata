2a22e78a226f82ba02aad638687e632aa188bcce
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-06-06 10:36:47 +0200

    Initial commit for strategy model implementation

---
+++ src/main/java/com/strategy/OverCrowdingStrategy.java
@@ -0,0 +1,20 @@
+package com.strategy;
+
+import com.LifeForm;
+
+import java.util.List;
+
+/**
+ * Created by vasilis on 6-6-17.
+ */
+public class OverCrowdingStrategy implements NextGenStrategy {
+    public boolean strategyApplies(LifeForm[][] grid, int posX, int posY) {
+        //TODO implementation
+        return false;
+    }
+
+    public List<LifeForm> identifyNeighbours(LifeForm[][] grid) {
+        //TODO implementation
+        return null;
+    }
+}
---
+++ src/main/java/com/strategy/UnderpopulationStrategy.java
@@ -0,0 +1,22 @@
+package com.strategy;
+
+import com.LifeForm;
+
+import java.util.List;
+
+/**
+ * Created by vasilis on 6-6-17.
+ */
+public class UnderpopulationStrategy implements NextGenStrategy {
+
+    public boolean strategyApplies(LifeForm[][] grid, int posX, int posY) {
+        boolean result = false;
+        //TODO implementation
+        return result;
+    }
+
+    public List<LifeForm> identifyNeighbours(LifeForm[][] grid) {
+        //TODO implementation
+        return null;
+    }
+}
