021be24b89c9d09ce72e0936ca5e933dafe027e8
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-06-08 09:50:15 +0200

    First Implementation for Preserve Strategy

--- src/main/java/com/strategy/PreserveStrategy.java
+++ src/main/java/com/strategy/PreserveStrategy.java
@@ -1,20 +1,31 @@
 package com.strategy;
 
 import com.LifeForm;
+import com.utils.NeighbourHelper;
 
+import java.awt.*;
 import java.util.List;
 
 /**
  * Created by vasilis on 6-6-17.
  */
 public class PreserveStrategy implements NextGenStrategy {
-    public boolean strategyApplies(LifeForm[][] grid, int posX, int posY) {
-        //TODO implementation
-        return false;
-    }
 
-    public List<LifeForm> identifyNeighbours(LifeForm[][] grid) {
-        //TODO implementation
-        return null;
+    public boolean strategyApplies(LifeForm[][] grid, int posX, int posY) {
+        boolean result = false;
+        Point myLifeFormLocation = new Point(posX, posY);
+        if (grid[myLifeFormLocation.x][myLifeFormLocation.y].isAlive()) {
+            List<LifeForm> neighbours = NeighbourHelper.identifyNeighbours(grid, myLifeFormLocation);
+            int amountOfAliveNeighbours = 0;
+            for (LifeForm lifeForm : neighbours) {
+                if (lifeForm.isAlive())
+                    amountOfAliveNeighbours++;
+            }
+            if (amountOfAliveNeighbours > 1 && amountOfAliveNeighbours < 4) {
+                result = true;
+            }
+
+        }
+        return result;
     }
 }
