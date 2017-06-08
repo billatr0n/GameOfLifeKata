c9dab010d7886409a47a67bd468eabac351a4b48
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-06-08 09:48:09 +0200

    Introduce failing unit tests for Overcrowding strategy

---
+++ src/test/java/com/strategy/OverCrowdingStrategyTest.java
@@ -0,0 +1,84 @@
+package com.strategy;
+
+import com.LifeForm;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vasilis on 7-6-17.
+ */
+@RunWith(JUnitParamsRunner.class)
+public class OverCrowdingStrategyTest {
+
+    OverCrowdingStrategy SUT;
+
+    @Before
+    public void setUp() throws Exception {
+    SUT = new OverCrowdingStrategy();
+
+    }
+    private static Object[] strategySuccessfulLocations() {
+        LifeForm[][] initialWorld = new LifeForm[][]{
+                {new LifeForm(false, "*", "-"), new LifeForm(true, "*", "_"), new LifeForm(false, "*", "-")},
+                {new LifeForm(true, "*", "_"), new LifeForm(true, "*", "_"), new LifeForm(false, "*", "-")},
+                {new LifeForm(true, "*", "-"), new LifeForm(true, "*", "-"), new LifeForm(false, "*", "-")}
+        };
+
+        return new Object[]{
+                new Object[]{initialWorld, 1,1},
+        };
+    }
+
+    private static Object[] nonStrategySuccessfulLocations() {
+        LifeForm[][] initialWorld = new LifeForm[][]{
+                {new LifeForm(false, "*", "-"), new LifeForm(true, "*", "_"), new LifeForm(false, "*", "-")},
+                {new LifeForm(true, "*", "_"), new LifeForm(true, "*", "_"), new LifeForm(false, "*", "-")},
+                {new LifeForm(true, "*", "-"), new LifeForm(true, "*", "-"), new LifeForm(false, "*", "-")}
+        };
+
+        return new Object[]{
+
+                new Object[]{initialWorld, 0,0},
+                new Object[]{initialWorld, 0,1},
+                new Object[]{initialWorld, 2,0},
+                new Object[]{initialWorld, 2,2},
+                new Object[]{initialWorld, 0,2},
+                new Object[]{initialWorld, 2,1},
+        };
+    }
+
+    @Test
+    @Parameters(method="strategySuccessfulLocations")
+    public void strategyApplies(LifeForm[][] grid, int posX , int posY){
+        //arrange
+        final boolean expected = true;
+
+        //act
+        final boolean actual = SUT.strategyApplies(grid,posX, posY);
+
+        //assert
+        Assert.assertEquals("Not the same",expected, actual);
+
+    }
+    @Test
+    @Parameters(method="nonStrategySuccessfulLocations")
+    public void strategyDoesNoApply(LifeForm[][] grid, int posX , int posY){
+        //arrange
+        final boolean expected = false;
+
+        //act
+        final boolean actual = SUT.strategyApplies(grid,posX, posY);
+
+        //assert
+        Assert.assertEquals("Not the same",expected, actual);
+
+    }
+
+}
