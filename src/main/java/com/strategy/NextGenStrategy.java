0957ad79f742d0a5f85f2b03495e979fc6840797
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-06-08 09:47:39 +0200

    Introduce Next Generation strategy Interface

--- src/main/java/com/strategy/NextGenStrategy.java
+++ src/main/java/com/strategy/NextGenStrategy.java
@@ -2,12 +2,10 @@ package com.strategy;
 
 import com.LifeForm;
 
-import java.util.List;
 
 /**
  * Created by vasilis on 6-6-17.
  */
 public interface NextGenStrategy {
      boolean strategyApplies(LifeForm[][] grid,int posX, int posY);
-    List<LifeForm> identifyNeighbours(LifeForm[][] grid);
 }
