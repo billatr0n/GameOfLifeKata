3b91500856b9cccae6c44a4b3477cf628653b9f9
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-06-12 11:34:47 +0200

    Add Failing tests for next Generation

--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -1,10 +1,14 @@
 import com.GameOfLife;
 import com.LifeForm;
+import com.strategy.*;
 import junitparams.JUnitParamsRunner;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vasilis on 30-5-17.
@@ -30,9 +34,15 @@ public class GameOfLifeTest {
                 {new LifeForm(false, "*", "-"), new LifeForm(false, "*", "-"), new LifeForm(false, "*", "-"), new LifeForm(false, "*", "-")}
         };
 
-        SUT = new GameOfLife(startingGrid);
+        List<NextGenStrategy> rules = new ArrayList<NextGenStrategy>();
+        rules.add(new OverCrowdingStrategy());
+        rules.add(new PreserveStrategy());
+        rules.add(new RebornStrategy());
+        rules.add(new UnderpopulationStrategy());
+        SUT = new GameOfLife(startingGrid, rules);
         //Provide the two-dimensional world;
     }
+
     @Test
     public void renderWorld() {
         //Arrange
@@ -40,9 +50,29 @@ public class GameOfLifeTest {
                 "-**-\n" +
                 "----\n";
         //Act
-        String actual = SUT.renderGrid(startingGrid);
+        String actual = SUT.renderWorld(startingGrid);
 
         //Assert
         Assert.assertEquals("The world needs to be rendered properly.", expected, actual);
+
+        SUT.createNextGeneration();
+        SUT.renderWorld(SUT.getWorld());
+    }
+
+    @Test
+    public void renderSecondGenWorld() {
+        //Arrange
+        String expected = "-**-\n" +
+                "-**-\n" +
+                "----\n";
+        //Act
+        SUT.createNextGeneration();
+        String actual = SUT.renderWorld(SUT.getWorld());
+        //Assert
+
+        Assert.assertEquals("The world needs to be rendered properly.", expected, actual);
+
+
+
     }
 }
